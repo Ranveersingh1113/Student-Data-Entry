aac829e7301523aee23596682bfcf3c69ac3764f
Ranveersingh1113 <161949661+Ranveersingh1113@users.noreply.github.com>
2025-04-19 20:49:24 +0530

    Create SearchOperationException.java

---
+++ studentdata/Exceptions/SearchOperationException.java
@@ -0,0 +1,6 @@
+package studentdata.exceptions;
+
+/** Base exception for Search operation errors. */
+public class SearchOperationException extends Exception {
+    public SearchOperationException(String message) { super(message); }
+}
