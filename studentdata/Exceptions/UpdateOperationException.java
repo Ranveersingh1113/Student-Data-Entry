2220a2d80971718410ec8ec64ecf2d0b0c992455
Ranveersingh1113 <161949661+Ranveersingh1113@users.noreply.github.com>
2025-04-19 20:50:39 +0530

    Create UpdateOperationException.java

---
+++ studentdata/Exceptions/UpdateOperationException.java
@@ -0,0 +1,6 @@
+package studentdata.exceptions;
+
+/** Base exception for Update operation errors. */
+public class UpdateOperationException extends Exception {
+    public UpdateOperationException(String message) { super(message); }
+}
